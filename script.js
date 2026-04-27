4f80e58ca11a0d0128ea470327fa8905f6898230
Mahmudul-UIU23 <mhasan2330897@bscse.uiu.ac.bd>
2026-04-28 00:08:03 +0600

    home page design created

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 //login functionality
 document.getElementById("loginButton").addEventListener("click", function(e) {
-    console.log("Login button clicked")
+    // ata dile bar bar reload hoibo na
+    e.preventDefault()
+    const DefaultMobileNumber = 123456789
+    const DefaultPin = 1234
+    
+    const mobileNumberValue = document.getElementById
+    ("mobile-number").value
+    const mobileNumberValueConverted = parseInt(mobileNumberValue)
+
+    const pinNumberValue = document.getElementById("pin-number").value
+    const pinNumberValueConverted = parseInt(pinNumberValue)
+
+   
+
+    if(mobileNumberValueConverted ===DefaultMobileNumber && pinNumberValueConverted === DefaultPin){
+        window.location.href="home.html"
+    }else{
+       alert("Wrong Username and password")
+    }
 });
     
